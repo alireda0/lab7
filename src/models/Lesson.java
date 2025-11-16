9464283c04b7c7a69b4527c71fb08af3cfb1b5b6
alireda0 <es.ali.wahiba2023@alexu.edu.eg>
2025-11-16 23:37:26 +0200

    course

--- src/models/Lesson.java
+++ src/models/Lesson.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package models;
 
-/**
- *
- * @author SHIKO
- */
+import java.util.List;
+
 public class Lesson {
-    
+
+    private int lessonId;
+    private String title;
+    private String content;
+    private List<String> resources;  // Optional (URLs, file paths, etc.)
+
+    // ----------------- Constructors -----------------
+
+    public Lesson() {}
+
+    public Lesson(int lessonId, String title, String content, List<String> resources) {
+        this.lessonId = lessonId;
+        this.title = title;
+        this.content = content;
+        this.resources = resources;
+    }
+
+    // ----------------- Getters -----------------
+
+    public int getLessonId() {
+        return lessonId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public List<String> getResources() {
+        return resources;
+    }
+
+    // ----------------- Setters -----------------
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setResources(List<String> resources) {
+        this.resources = resources;
+    }
 }
